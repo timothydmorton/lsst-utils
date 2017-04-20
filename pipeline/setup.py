f252454771a290c0f5afc1a4db26cb517b4c86cf
Timothy Morton <tdm@astro.princeton.edu>
2017-04-20 16:45:15 -0400

    added files

---
+++ pipeline/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os, sys
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name = "pipeline",
+    version = 0.0,
+    description = "Launch LSST pipeline runs in one step.",
+    long_description = readme(),
+    author = "Timothy D. Morton",
+    author_email = "tdm@astro.princeton.edu",
+    url = "https://github.com/timothydmorton/lsst-utils/pipeline",
+    packages = find_packages(),
+    scripts = ['scripts/runPipeline.py']
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Science/Research',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering :: Astronomy'
+      ],
+    zip_safe=False
+)
