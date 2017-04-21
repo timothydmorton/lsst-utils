9cde218cf157409b9cb52686aa12068f218744a3
Timothy Morton <tdm@astro.princeton.edu>
2017-04-20 22:50:07 -0400

    switched arg

--- pipeline/scripts/runPipeline.py
+++ pipeline/scripts/runPipeline.py
@@ -6,10 +6,10 @@ from pipeline import Pipeline
 parser = argparse.ArgumentParser('Run a pipeline')
 parser.add_argument('configfile', help='yaml file')
 parser.add_argument('--test', action='store_true')
-parser.add_argument('--parallel', action='store_true')
+parser.add_argument('--serial', action='store_true')
 
 args = parser.parse_args()
 
 pipe = Pipeline(args.configfile)
-pipe.run(test=args.test, parallel=args.parallel)
+pipe.run(test=args.test, parallel=not args.serial)
 
