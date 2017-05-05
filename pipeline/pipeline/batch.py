335117239ff34efa79056bf3fca4f344bb3203ed
Timothy Morton <tdm@astro.princeton.edu>
2017-05-05 00:51:24 -0400

    DOTALL for real

--- pipeline/pipeline/batch.py
+++ pipeline/pipeline/batch.py
@@ -47,6 +47,6 @@ def get_pipeline_status(name):
     with open(pipe_logfile) as fin:
         file_str = fin.read()
 
-    pattern = re.compile('='*30 + '\n' + '(TEST\n)?' + '(20\d\d-\d\d-\d\d.*)\n' + '.*' + '='*30)# + '\n' + '(.*)' + '($|=)')
-    m = re.findall(pattern, file_str, flags=re.DOTALL)
+    pattern = re.compile('='*30 + '\n' + '(TEST\n)?' + '(20\d\d-\d\d-\d\d.*)\n' + '.*' + '='*30, flags=re.DOTALL)# + '\n' + '(.*)' + '($|=)')
+    m = re.findall(pattern, file_str)
     return pattern, m, file_str
