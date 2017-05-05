11058db07d2862e953fac383a82bec55c65b4882
Timothy Morton <tdm@astro.princeton.edu>
2017-05-05 02:10:22 -0400

    get rid of old thing

--- pipeline/pipeline/batch.py
+++ pipeline/pipeline/batch.py
@@ -76,7 +76,6 @@ def get_pipeline_status(name, info=('jobid','State','Elapsed','start','end','exi
         keep_indices = [i for i in df.index if re.search('^\d+$', str(i))]
 
         df = df.ix[keep_indices]
-        df['job'] = jobs
         results.append(df)
 
 
