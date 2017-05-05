b26767aaddeae55d56595dff1aecd215c57db586
Timothy Morton <tdm@astro.princeton.edu>
2017-05-05 02:01:44 -0400

    debug

--- pipeline/pipeline/batch.py
+++ pipeline/pipeline/batch.py
@@ -72,6 +72,8 @@ def get_pipeline_status(name, info=('jobid','State','Elapsed','start','end','exi
         keep_indices = [i for i in df.index if re.search('^\d+$', str(i))]
 
         df = df.ix[keep_indices]
+        print(jobs)
+        print(ids)
         print(df)
         df['job'] = jobs
         results.append(df)
