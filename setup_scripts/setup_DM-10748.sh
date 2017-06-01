11fcd03ab97c8a6015753dd313a7c774ee162e9b
Timothy Morton <tdm@astro.princeton.edu>
2017-06-01 09:36:21 -0500

    lsst-dev specific

--- setup_scripts/setup_DM-10748.sh
+++ setup_scripts/setup_DM-10748.sh
@@ -1,5 +1,7 @@
 CWD=`pwd`
-export LSST_STACK_DIR=/software/lsstsw/stack
+if [ "$HOST" == "lsst-dev01.ncsa.illinois.edu" ]; then
+    export LSST_STACK_DIR=/software/lsstsw/stack
+fi
 
 . setup_stack.sh
 setup_local meas_mosaic tickets/DM-10737
