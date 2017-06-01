6d90acf40eeb1f507dad25295458b860754a263f
Timothy Morton <tdm@astro.princeton.edu>
2017-06-01 09:30:08 -0500

    added files

--- setup_scripts/setup_DM-10748.sh
+++ setup_scripts/setup_DM-10748.sh
@@ -1,8 +1,7 @@
 CWD=`pwd`
-
 export LSST_STACK_DIR=/software/lsstsw/stack
+
 . setup_stack.sh
-setup lsst_distrib -t current
-setup_local meas_mosaic
+setup_local meas_mosaic tickets/DM-10737
 
 cd $CWD
