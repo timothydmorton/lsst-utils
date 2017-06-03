49c37582e297066609cab029be1aa786f4dfb263
Timothy Morton <tdm@astro.princeton.edu>
2017-06-02 20:27:49 -0400

    no need to type .yaml for file

--- pipeline/scripts/runPipeline.py
+++ pipeline/scripts/runPipeline.py
@@ -10,6 +10,11 @@ parser.add_argument('--serial', action='store_true')
 
 args = parser.parse_args()
 
-pipe = Pipeline(args.configfile)
+if not args.configfile.endswith('.yaml'):
+    file = args.configfile + '.yaml'
+else:
+    file = args.configfile
+
+pipe = Pipeline(file)
 pipe.run(test=args.test, parallel=not args.serial)
 
